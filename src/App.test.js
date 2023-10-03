438c5d95d6b7b8f457e1996c0ebe2490ed301834
nikhilrajsv054 <70802832+nikhilrajsv054@users.noreply.github.com>
2023-10-03 21:40:07 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import configuretheme from './configuretheme';
+import * as date from './date';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock the getThemes function
+jest.mock('./date', () => ({
+  getThemes: jest.fn(),
+}));
+
+describe('configuretheme function', () => {
+  afterEach(() => {
+    // Clear mock calls after each test
+    jest.clearAllMocks();
+  });
+
+  it('should call date.getThemes with the correct arguments for DateTime type', () => {
+    // Arrange
+    const theme = { /* your theme object here */ };
+    const instance = { type: 'DateTime' };
+
+    // Act
+    configuretheme.call(instance, theme);
+
+    // Assert
+    expect(date.getThemes).toHaveBeenCalledWith(theme);
+  });
+
+  it('should log a message for unknown type', () => {
+    // Arrange
+    const theme = { /* your theme object here */ };
+    const instance = { type: 'UnknownType' };
+    
+    // Mock the console.log function
+    const consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+    // Act
+    configuretheme.call(instance, theme);
+
+    // Assert
+    expect(date.getThemes).not.toHaveBeenCalled(); // Ensure getThemes is not called
+    expect(consoleLogMock).toHaveBeenCalledWith(`no theme ${instance.type}`);
+
+    // Restore the original console.log function
+    consoleLogMock.mockRestore();
+  });
 });
+
