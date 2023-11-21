f25470b51335a672077e1d22da623f345c5a9e62
nikhilrajsv054 <70802832+nikhilrajsv054@users.noreply.github.com>
2023-11-21 13:03:18 +0530

    Update Comments.js

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,171 +1,92 @@
-#----------------------------------------------------------------------------------------
+import React, { useState } from 'react';
+import './App.css';
 
-  To implement Redux Toolkit middleware for handling Electron IPC communication and updating JSON data with posts, you can follow these steps:
+const Shapes = () => {
+  const [selectedShape, setSelectedShape] = useState(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [shapes, setShapes] = useState([]);
+  const [activeShapeIndex, setActiveShapeIndex] = useState(null);
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
 
-Install the required dependencies:
+  const handleShapeSelection = (shape) => {
+    setSelectedShape(shape);
+  };
 
-You'll need redux-thunk and fs (Node.js file system module) to handle asynchronous actions and file operations. Install them using npm or yarn:
-
-Copy code
-npm install redux-thunk fs-extra
-Create a new Redux Toolkit middleware to handle IPC communication and JSON data updates. Let's call it ipcMiddleware.js:
-
-javascript
-Copy code
-// ipcMiddleware.js
-const { ipcRenderer } = window.require('electron');
-const fs = window.require('fs-extra');
-
-export const ipcMiddleware = (store) => (next) => (action) => {
-  switch (action.type) {
-    case 'posts/addPost': {
-      // Send an IPC request to add the post data
-      ipcRenderer.invoke('addPost', action.payload).then((response) => {
-        // Dispatch a Redux action with the response data
-        store.dispatch({ type: 'posts/addPostSuccess', payload: response });
-      });
-      break;
+  const handleCanvasMouseDown = (e) => {
+    if (selectedShape) {
+      setIsDrawing(true);
+      setDragStart({ x: e.clientX, y: e.clientY });
+      const newShape = {
+        type: selectedShape,
+        top: e.clientY,
+        left: e.clientX,
+        width: 0,
+        height: 0,
+      };
+      setShapes([...shapes, newShape]);
+      setActiveShapeIndex(shapes.length);
     }
-    case 'posts/editPost': {
-      // Send an IPC request to edit the post data
-      ipcRenderer.invoke('editPost', action.payload).then((response) => {
-        // Dispatch a Redux action with the response data
-        store.dispatch({ type: 'posts/editPostSuccess', payload: response });
+  };
+
+  const handleShapeDragStart = (index, e) => {
+    e.preventDefault(); 
+    e.stopPropagation();
+    setDragStart({ x: e.clientX, y: e.clientY });
+    setActiveShapeIndex(index);
+  };
+
+  const handleShapeDrag = (e) => {
+    if (activeShapeIndex !== null && isDrawing) {
+      const updatedShapes = shapes.map((shape, index) => {
+        if (index === activeShapeIndex) {
+          const offsetX = e.clientX - dragStart.x;
+          const offsetY = e.clientY - dragStart.y;
+          return {
+            ...shape,
+            width: Math.abs(offsetX),
+            height: Math.abs(offsetY),
+          };
+        }
+        return shape;
       });
-      break;
+      setShapes(updatedShapes);
     }
-    default:
-      return next(action);
-  }
+  };
+  
+
+  const handleShapeDragEnd = () => {
+    setIsDrawing(false);
+    setActiveShapeIndex(null);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <button onClick={() => handleShapeSelection('circle')}>Circle</button>
+        <button onClick={() => handleShapeSelection('rectangle')}>Rectangle</button>
+      </nav>
+      <div
+        className="canvas"
+        onMouseDown={handleCanvasMouseDown}
+        onMouseMove={handleShapeDrag}
+        onMouseUp={handleShapeDragEnd}
+      >
+        {shapes.map((shape, index) => (
+          <div
+            key={index}
+            className={`shape ${shape.type}`}
+            style={{
+              top: shape.top,
+              left: shape.left,
+              width: shape.width,
+              height: shape.height,
+            }}
+            onMouseDown={(e) => handleShapeDragStart(index, e)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default ipcMiddleware;
-This middleware listens for actions related to adding and editing posts and sends corresponding IPC requests. When the IPC requests are successful, it dispatches Redux actions to update the state.
-
-Update your Redux store configuration to include this middleware:
-
-javascript
-Copy code
-// store.js
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import postsReducer from './Slices/postsSlice';
-import authReducer from './Slices/authSlice';
-import { ipcMiddleware } from '../middleware/ipcMiddleware'; // Import the custom middleware
-
-const middleware = [...getDefaultMiddleware(), ipcMiddleware];
-
-const store = configureStore({
-  reducer: {
-    posts: postsReducer,
-    auth: authReducer,
-  },
-  middleware,
-});
-
-export default store;
-Modify your postsSlice.js to handle the new success actions:
-
-javascript
-Copy code
-// postsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
-
-const postsSlice = createSlice({
-  // ... other slice configuration ...
-
-  reducers: {
-    // ... other reducers ...
-
-    addPostSuccess: (state, action) => {
-      state.push(action.payload); // Add the new post to the state
-    },
-
-    editPostSuccess: (state, action) => {
-      const { id, title, content } = action.payload;
-      const postIndex = state.findIndex((post) => post.id === id);
-      if (postIndex !== -1) {
-        // Update the post with the edited title and content
-        state[postIndex].title = title;
-        state[postIndex].content = content;
-      }
-    },
-  },
-});
-
-// ... export other actions and selectors ...
-
-export default postsSlice.reducer;
-Finally, update your Electron main process to handle IPC requests and update the JSON data file accordingly. Here's a simplified example of the main process code:
-
-javascript
-Copy code
-// main.js (Electron main process)
-const { app, BrowserWindow, ipcMain } = require('electron');
-const fs = require('fs-extra');
-const path = require('path');
-
-let mainWindow;
-
-// Create the main window
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    // ... window configuration ...
-  });
-
-  // Load your main HTML file
-  mainWindow.loadFile('index.html');
-
-  // Handle IPC requests to add a new post
-  ipcMain.handle('addPost', async (event, newPost) => {
-    try {
-      // Read the existing JSON data file
-      const jsonData = await fs.readJson('data.json');
-
-      // Generate a unique ID for the new post (you can use a library like `uuid`)
-      newPost.id = Date.now();
-
-      // Add the new post to the JSON data
-      jsonData.push(newPost);
-
-      // Write the updated JSON data back to the file
-      await fs.writeJson('data.json', jsonData);
-
-      return newPost;
-    } catch (error) {
-      console.error('Error adding post:', error);
-      throw error;
-    }
-  });
-
-  // Handle IPC requests to edit an existing post
-  ipcMain.handle('editPost', async (event, editedPost) => {
-    try {
-      // Read the existing JSON data file
-      const jsonData = await fs.readJson('data.json');
-
-      // Find the index of the post to edit by its ID
-      const postIndex = jsonData.findIndex((post) => post.id === editedPost.id);
-
-      if (postIndex !== -1) {
-        // Update the post's title and content
-        jsonData[postIndex].title = editedPost.title;
-        jsonData[postIndex].content = editedPost.content;
-
-        // Write the updated JSON data back to the file
-        await fs.writeJson('data.json', jsonData);
-
-        return editedPost;
-      } else {
-        throw new Error('Post not found');
-      }
-    } catch (error) {
-      console.error('Error editing post:', error);
-      throw error;
-    }
-  });
-});
-
-// ... other Electron app logic ...
-This code sets up IPC handlers to add and edit posts in the JSON data file. When a request is made from the renderer process, it reads, updates, and writes back the JSON data file as needed.
-
-Please adapt this example to your specific project structure and requirements. Additionally, consider adding error handling and validation as needed for robustness.
+export default Shapes;
